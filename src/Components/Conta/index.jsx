bf6e313fc706c4165503707044301fc46aafe2e4
HenriqueCSaraiva <henriquesaraiva24@gmail.com>
2021-11-16 16:24:26 -0300

    first commit

---
+++ src/Components/Conta/index.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+
+import privado from "../../assets/images/privado.svg";
+import olho_icone from "../../assets/images/olho.svg";
+import dinheiro from "../../assets/images/dinheiro.svg";
+import { Botao, Box, Detalhe, Icone, Saldo, IconeTema} from '../../Components/UI/index'
+
+const Conta = () => {
+  const [toggleState, untoggle] = useState(true);
+
+  const toggleHandler = () => {
+    untoggle((toggleState) => !toggleState);
+  };
+
+  return (
+    <Box>
+      <h2>Conta</h2>
+      <div style={{ fontSize: "26px", padding: "20px 0" }}>
+        Saldo disponível{" "}
+        <Detalhe>
+          <IconeTema src={dinheiro} alt="Ícone Saldo" />
+        </Detalhe>
+        {toggleState ? (
+          <Saldo>
+            <Detalhe>R$</Detalhe> 0,00{" "}
+          </Saldo>
+        ) : null}
+      </div>
+
+      <Botao className="btn" onClick={toggleHandler}>
+        <IconeMargin
+          src={toggleState ? privado : olho_icone}
+          alt="Privacidade do Saldo"
+        />
+      </Botao>
+    </Box>
+  );
+};
+
+const IconeMargin = styled(Icone)`
+  margin-top: 2px
+`
+
+export default Conta;
